5616e496e694adad4408758e9af81390e10d3405
Darwin Rodriguez <darwinrv94@gmail.com>
2023-08-20 14:13:05 -0600

    Visualizar lista de actores

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -24,6 +24,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
@@ -124,10 +125,74 @@ class _MovieDetails extends StatelessWidget {
           )
         ),
 
-        // Todo: mostrar actores ListView
+        // Actores
+        _ActorsByMovie(movieId: movie.id.toString()),
 
-        const SizedBox(height: 100)
+        const SizedBox(height: 50)
       ]
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const _ActorsByMovie({
+    required this.movieId
+  });
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(
+        strokeWidth: 2
+      );
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Actor photo
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover
+                  )
+                ),
+
+                const SizedBox(height: 5),
+
+                // Nombre
+                Text(actor.name, maxLines: 2),
+                Text(
+                  actor.character ?? '',
+                  maxLines: 2,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  overflow: TextOverflow.ellipsis,
+                )
+              ]
+            ),
+          );
+        }
+      ),
     );
   }
 }
@@ -152,11 +217,11 @@ class _CustomSliverAppBar extends StatelessWidget {
           horizontal: 10,
           vertical: 5
         ),
-        title: Text(
+        /* title: Text(
           movie.title,
           style: const TextStyle(fontSize: 20),
           textAlign: TextAlign.start
-        ),
+        ), */
         background: Stack(
           children: [
             SizedBox.expand(
