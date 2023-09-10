da04aae5ebe2b5f010b905e2e7b6e22e4f71b8a0
Darwin Rodriguez <darwinrv94@gmail.com>
2023-09-10 14:11:22 -0600

    Page view implementado

--- lib/presentation/widgets/movies/movie_poster_link.dart
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +15,21 @@ class MoviePosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
+
     return FadeInUp(
+      from: random.nextInt(100) + 80,
+      delay: Duration(milliseconds: random.nextInt(450) + 0 ),
       child: GestureDetector(
         onTap: () => context.push('/home/0/movie/${ movie.id }'),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.posterPath)
+          child: FadeInImage(
+            height: 180,
+            fit: BoxFit.cover,
+            placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+            image: NetworkImage(movie.posterPath),
+          ),
         ),
       ),
     );
